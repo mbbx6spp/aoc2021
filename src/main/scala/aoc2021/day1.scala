ea2297e58bdea3cf9c2b4d7cd754814138dc847a
Susan Potter <me@susanpotter.net>
2021-12-01 06:29:22 +0000

    Day1 solution for part 1 and part 2

--- src/main/scala/aoc2021/day1.scala
+++ src/main/scala/aoc2021/day1.scala
@@ -1,7 +1,44 @@
 package aoc2021
 
+import cats._
+import cats.syntax._
+import cats.implicits._
 import cats.effect.{IO, IOApp}
+import fs2._
+import fs2.io.file.{Files, Path}
+import scala.util.Try
+
+case class State(previous: Option[Int], increaseCount: Int) {
+  def isEmpty: Boolean = previous.fold(increaseCount == 0)(_ => false)
+}
+object State {
+  def empty: State = State(None, 0)
+}
 
 object Day1 extends IOApp.Simple {
-  def run = IO.println("TODO: Day1")
+
+  def foldSuccess(elem: Int)(state: State): State = 
+    state match {
+      case State(None, n) => State(Some(elem), n)
+      case State(Some(prev), n) if elem > prev => State(Some(elem), n+1)
+      case State(_, n) => State(Some(elem), n)
+    }
+  
+  def foldFailure(err: Throwable): State = State.empty
+
+  def solution(n: Int) = 
+    Files[IO]
+      .readAll(Path("./data/day1part1"))
+      .through(text.utf8.decode)
+      .through(text.lines)
+      .map((s: String) => Try(s.toInt).toEither)
+      .sliding(n)
+      .fold(State(None, 0))((state, chunk) => chunk.toList.sequence.map(_.sum).fold(foldFailure, foldSuccess(_)(state)))
+      .evalMap((line: State) => IO.println(line))
+      .compile
+      .drain
+
+  def part1 = solution(1)
+  def part2 = solution(3)
+  def run = part2
 }
